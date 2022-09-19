45cbf003dbcce4e9612ac3f5c157e3ad36582b5d
Kepha Kibet <kibet.kepha@live.com>
2022-09-19 21:41:22 +0300

    style header with bootstap 5

--- chooseme/src/header.js
+++ chooseme/src/header.js
@@ -3,12 +3,15 @@ import React from 'react';
 const Header = ({selectedTeam, teamMemberCount}) => {
   
   return (
-    <header>
-      <h1>
-       ChooseMe Team Member Sllocation
-      </h1>
-  <h3>{selectedTeam} has {teamMemberCount} members</h3>
-  
+    <header className="container" >
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className ="col-8 row ">
+          <h1>
+          ChooseMe Team Member Allocation
+          </h1>
+          <h3>{selectedTeam} has {teamMemberCount} members</h3>
+          </div>
+        </div>
     </header>
     )
 }
