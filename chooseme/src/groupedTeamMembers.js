bb6a7cc7e504eeef51389ae4ffaa1e84a35c1e0b
Kepha Kibet <kibet.kepha@live.com>
2022-09-21 23:34:07 +0300

    groupedTeamMembers component

---
+++ chooseme/src/groupedTeamMembers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const GroupedTeamMembers= () => {
+  
+  return (
+    <main className="container" >
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className ="col-8 row ">
+          <h1> Grouped Team Members</h1>
+          </div>
+        </div>
+    </main>
+    )
+}
+
+
+export default GroupedTeamMembers;
