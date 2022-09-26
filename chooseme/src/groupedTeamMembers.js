b3107d480cc58a43403f1c26df5911df85516763
Kepha Kibet <kibet.kepha@live.com>
2022-09-26 20:08:02 +0300

    minor bug fixes

--- chooseme/src/groupedTeamMembers.js
+++ chooseme/src/groupedTeamMembers.js
@@ -4,8 +4,7 @@ import { useState } from 'react';
 const GroupedTeamMembers = ({ employees, selectedTeam, setTeam }) => {
   
   const [groupedEmployees, setGroupedData] = useState(groupTeamMembers())
-  function groupTeamMembers()
-  {
+  function groupTeamMembers() {
     var teams = []
 
     var phoenixMembers = employees.filter((employee) => employee.teamName === 'Phoenix');
@@ -22,22 +21,42 @@ const GroupedTeamMembers = ({ employees, selectedTeam, setTeam }) => {
 
     var royalsMembers = employees.filter((employee) => employee.teamName === 'Royals');
     var royals = { team: 'Royals', members: royalsMembers, collapsed: selectedTeam === 'Royals' ? false : true };
-    teams.push(royals)
+    teams.push(royals);
+
+    function handleTeamClick(event) {
+      
+    }
   
     return (
       <main className="container" >
         {
           groupedEmployees.map((item) => {
             return (
-              <div key ={item.team} className='card mt-2' style
+              <div key={item.team} className='card mt-2' style={{ cursor: 'pointer' }}>
+                <h4 id={item.team} className='card-header text-secondary bg-white'> Team Name : {item.team} </h4>
+                <div id={'collapse' + item.team}
+                  className={item.collapsed === true ? 'collapsed' : ''}>
+                  <hr />
+                  {
+                    item.members.map(member => {
+                      return (
+                        <div className='mt-2'>
+                          <h5 className='card-tittle mt-2'>
+                            <span className='text-dark'> FullName :{member.fullName} </span> </h5>
+                          <p> Designation :{member.designation}</p>
+      
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div >
             )
           })
         }
-        
       </main>
     )
   }
-  
 }
 
 
